29e7447f8fae2a902e5bec1b66fc9db23e203e56
Dávid Halász <dhalasz@redhat.com>
2016-05-16 17:34:33 +0200

    Converted from version 3.3.5, but will be released as 3.3.6

--- lib/patternfly-sass/version.rb
+++ lib/patternfly-sass/version.rb
@@ -1,4 +1,4 @@
 module Patternfly
-  VERSION = '3.3.6-alpha'
-  PATTERNFLY_SHA = '37221625755ab0d657d1b60f712c8ead4b166bff'
+  VERSION = '3.3.6'
+  PATTERNFLY_SHA = 'e53ce5d07b0a9e8309513b24bf50c0db3d5f3b58'
 end
