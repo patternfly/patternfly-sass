07069f775e3636bba56747f326c7de45cf4f82af
Dávid Halász <dhalasz@redhat.com>
2017-01-16 10:43:29 +0100

    Converted from upstream version 3.17.0

--- lib/patternfly-sass/version.rb
+++ lib/patternfly-sass/version.rb
@@ -1,4 +1,4 @@
 module Patternfly
-  VERSION = '3.15.0'
-  PATTERNFLY_SHA = '2eda227cf8a967c8a39bba84a631f9a5ff580bfc'
+  VERSION = '3.17.0'
+  PATTERNFLY_SHA = 'f01f390c0667096e0a4edeb31a6d2f7d440b5556'
 end
